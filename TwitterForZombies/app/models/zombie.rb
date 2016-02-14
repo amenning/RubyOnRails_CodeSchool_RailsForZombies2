b4cd20f39a1e7493125c00be16f0d9d21c3fded9
Andrew Menning <menning23@gmail.com>
2016-02-14 09:35:19 -0500

    Update Mailer

--- TwitterForZombies/app/models/zombie.rb
+++ TwitterForZombies/app/models/zombie.rb
@@ -1,3 +1,11 @@
 class Zombie < ActiveRecord::Base
   has_one :brain, dependent: :destroy
+  after_save :decomp_change_notification, if: :decomp_changed?
+  
+  private
+  
+  def decomp_change_notification
+    ZombieMailer.decomp_change(self).deliver
+  end
+  
 end
